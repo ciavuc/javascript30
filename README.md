3710f71f39bffa9b42d497e92b3b6352de180ed3
Joe Ciavucco <joe.ciavucco@dblx.co.uk>
2020-05-17 19:21:03 +0100

    Added timeout, styling on sound and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# JS30 - Drumkit
+Following the [30 day Vanilla JS challenge](https://github.com/wesbos/JavaScript30) by Wesbos
+
+Netlify: [Ciavuc.co - JS30 - 01 - Drumkit](https://amazing-kare-d58ebf.netlify.app/)
+
+A drumkit that takes a key input, uses that key to play an associated sound. 
--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    if (audio){
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if (audio) {
+        audio.currentTime = 0;
         audio.play();
+        key.classList.add('playing');
+        setTimeout(() => {
+            key.classList.remove('playing');
+        }, 100);
     }
 });
